29204819c10bfeea5a5330623c19a00a5b899e90
baka <babylovekenshi@gmail.com>
2025-03-05 17:35:42 +0700

    Update

--- API_VCBPayment/Program.cs
+++ API_VCBPayment/Program.cs
@@ -6,7 +6,8 @@ using System.Security.Cryptography.X509Certificates;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var cert = new X509Certificate2("/home/user/certs/server.crt", "/home/user/certs/server.key");
+//var cert = new X509Certificate2("/home/user/certs/server.crt", "/home/user/certs/server.key");
+var cert = new X509Certificate2("/etc/ssl/private/SimonERP_Hana.cer", "/etc/ssl/private/SimonERP_Hana.key");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
