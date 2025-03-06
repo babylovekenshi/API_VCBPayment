19a1ff7ec335e5ab96370c8482f519b8a99d12f9
baka <babylovekenshi@gmail.com>
2025-03-06 21:36:31 +0700

    update Log

--- API_VCBPayment/Program.cs
+++ API_VCBPayment/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //var cert = new X509Certificate2("/home/user/certs/server.crt", "/home/user/certs/server.key");
-var cert = new X509Certificate2("/etc/ssl/private/SimonERP_Hana.cer", "/etc/ssl/private/SimonERP_Hana.key");
+var cert = new X509Certificate2("/etc/ssl/private/SimonERP_Hana.cer", "");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,7 +20,13 @@ builder.WebHost.ConfigureKestrel(options =>
     });
 });
 var app = builder.Build();
-
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+app.Use(async (context, next) =>
+{
+    logger.LogInformation($"Request: {context.Request.Method} {context.Request.Path}");
+    await next.Invoke();
+    logger.LogInformation($"Response: {context.Response.StatusCode}");
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
